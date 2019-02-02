849e6ef1acd1aa09f032e413cf6b9f325eac37e2
monmartius <monmartius@gmail.com>
2019-02-02 21:12:11 +0700

    Webpack + Babel!

---
+++ dist/main.js
@@ -0,0 +1,100 @@
+
+
+
+
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/*!**********************!*\
+/************************************************************************/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/
+/******/ 			exports: {}
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		// Check if module is in cache
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		// Execute the module function
+/******/ 		// Flag the module as loaded
+/******/ 		// Return the exports of the module
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		if(installedModules[moduleId]) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		if(mode & 8) return value;
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 		module.l = true;
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		return getter;
+/******/ 		return module.exports;
+/******/ 		return ns;
+/******/ 		var getter = module && module.__esModule ?
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 		var ns = Object.create(null);
+/******/ 		}
+/******/ 		}
+/******/ 		}
+/******/ 		};
+/******/ 	// __webpack_public_path__
+/******/ 	// create a fake namespace object
+/******/ 	// define __esModule on exports
+/******/ 	// define getter function for harmony exports
+/******/ 	// expose the module cache
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	// Load entry module and return exports
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	// The module cache
+/******/ 	// The require function
+/******/ 	__webpack_require__.c = installedModules;
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 	__webpack_require__.m = modules;
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ 	var installedModules = {};
+/******/ 	}
+/******/ 	};
+/******/ 	};
+/******/ 	};
+/******/ 	};
+/******/ (function(modules) { // webpackBootstrap
+/******/ ({
+/******/ })
+/******/ });
+/***/ "./src/index.js":
+/***/ (function(module, exports) {
+/***/ })
+eval("var a = 1;\n\n//# sourceURL=webpack:///./src/index.js?");
